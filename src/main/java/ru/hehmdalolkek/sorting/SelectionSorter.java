dff353af0948988c7c4085c58b7574d13b68b96e
hehmdalolkek <badeha.inna@yandex.ru>
2024-10-13 11:39:30 +0400

    added selection sort

---
+++ src/main/java/ru/hehmdalolkek/sorting/SelectionSorter.java
@@ -0,0 +1,38 @@
+package ru.hehmdalolkek.sorting;
+
+/**
+ * The class implements the selection sort algorithm.
+ * The number of the minimum value in the current list is searched for.
+ * This value is exchanged with the value of the first unsorted position.
+ * The tail of the list is sorted, excluding already sorted elements from consideration.
+ * Search speed - O(n2).
+ *
+ * @author Inna Badekha
+ */
+public class SelectionSorter implements Sorter {
+
+    /**
+     * Sorts the {@code int} array by selection sort and returns the new sorted array.
+     *
+     * @param array the array to sort
+     * @return new sorted array
+     */
+    @Override
+    public int[] sort(int[] array) {
+        int[] result = array.clone();
+
+        for (int i = 0; i < result.length - 1; i++) {
+            int indexOfMinValue = i;
+            for (int j = i + 1; j < result.length; j++) {
+                if (result[j] < result[indexOfMinValue]) {
+                    indexOfMinValue = j;
+                }
+            }
+            int temp = result[i];
+            result[i] = result[indexOfMinValue];
+            result[indexOfMinValue] = temp;
+        }
+
+        return result;
+    }
+}
