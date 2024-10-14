af2008bd88f05dad7d1c27ac23a3d0c80cc7f162
hehmdalolkek <badeha.inna@yandex.ru>
2024-10-14 13:54:48 +0400

    added functionality for getting test parameters via command-line args

--- src/main/java/ru/hehmdalolkek/Main.java
+++ src/main/java/ru/hehmdalolkek/Main.java
@@ -4,6 +4,7 @@ import ru.hehmdalolkek.sorting.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * The main class that runs testing of all testing algorithms in the application.
@@ -13,28 +14,51 @@ import java.util.List;
 public class Main {
 
     /**
-     * Specifies the initial length of the array when testing.
+     * Default count of arrays for testing one sorting algorithm.
      */
-    private final static int INITIAL_ARRAY_SIZE = 100;
+    private static int countOfArrays = 3;
 
     /**
-     * Number of arrays for testing one sorting algorithm.
+     * Specifies the default initial length of the array when testing.
      */
-    private final static int COUNT_OF_ARRAYS = 3;
+    private static int initialArraySize = 100;
 
     /**
      * Entry point to the application.
-     * Creates test data {@code TestData}, gets all testing algorithms and starts testing of each algorithm.
+     * Gets values of {@code countOfArrays} and {@code initialArraySize}
+     * from command-line arguments or uses default values.
+     * Creates test data {@code TestData}.
+     * Gets all test algorithms and starts testing each algorithm.
      *
-     * @param args
+     * @param args array with values for fields {@code countOfArrays} and {@code initialArraySize}
      */
     public static void main(String[] args) {
-        TestData testData = new TestData(INITIAL_ARRAY_SIZE, COUNT_OF_ARRAYS);
-        List<Sorter> sorters = getAllSorters();
+        setParametersOfTest(args);
+        TestData testData = new TestData(initialArraySize, countOfArrays);
 
-        for (Sorter sorter : sorters) {
+        for (Sorter sorter : getAllSorters()) {
             SorterTester tester = new SorterTester(testData, sorter);
             tester.runTests();
+        }
+    }
+
+    /**
+     * Gets {@code args} and sets them
+     * to replace the default values of
+     * {@code countOfArrays} and {@code initialArraySize}.
+     *
+     * @param args array with values for fields {@code countOfArrays} and {@code initialArraySize}
+     */
+    private static void setParametersOfTest(String[] args) {
+        try {
+            if (args.length > 0 && !args[0].isEmpty()) {
+                countOfArrays = Integer.parseInt(args[0]);
+            }
+            if (args.length > 1 && !args[1].isEmpty()) {
+                initialArraySize = Integer.parseInt(args[1]);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid value passed.");
         }
     }
 
